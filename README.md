e6837f6ac098d802f0008820ec641e71987c8a00
aoirint <aoirint@gmail.com>
2022-03-18 07:21:37 +0900

    add user id, role args

--- README.md
+++ README.md
@@ -14,7 +14,8 @@ source venv/Scripts/activate
 
 pip3 install -r scripts/requiments.txt
 
-python3 scripts/create_token.py
+python3 scripts/create_token.py --user_id 1 --role sensor
+python3 scripts/create_token.py --user_id 2 --role client
 ```
 
 ### Generate requirements.txt
--- scripts/create_token.py
+++ scripts/create_token.py
@@ -1,29 +1,47 @@
-from dotenv import load_dotenv
-load_dotenv()
-
 import os
 import time
 import json
 import jwt
 
-jwt_secret = json.loads(os.environ['HASURA_GRAPHQL_JWT_SECRET'])
-secret_key = jwt_secret['key']
-
-# https://hasura.io/docs/latest/graphql/core/auth/authentication/jwt.html
-encoded_jwt = jwt.encode(
-  payload={
-    'sub': '1',
-    'name': 'user',
-    'admin': False,
-    'iat': int(time.time()),
-    'https://hasura.io/jwt/claims': {
-      'x-hasura-allowed-roles': 'user',
-      'x-hasura-default-role': 'user',
-      'x-hasura-user-id': '1',
+def create_token(
+  secret_key: str,
+  user_id: str,
+  role: str,
+) -> str:
+  # https://hasura.io/docs/latest/graphql/core/auth/authentication/jwt.html
+  encoded_jwt = jwt.encode(
+    payload={
+      'sub': user_id,
+      'name': role,
+      'admin': False,
+      'iat': int(time.time()),
+      'https://hasura.io/jwt/claims': {
+        'x-hasura-allowed-roles': [role],
+        'x-hasura-default-role': role,
+        'x-hasura-user-id': user_id,
+      },
     },
-  },
-  key=secret_key,
-  algorithm='HS256',
-)
+    key=secret_key,
+    algorithm='HS256',
+  )
+  return encoded_jwt
 
-print(encoded_jwt)
+
+if __name__ == '__main__':
+  from dotenv import load_dotenv
+  load_dotenv()
+
+  import argparse
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--user_id', type=str, required=True)
+  parser.add_argument('--role', type=str, required=True)
+  args = parser.parse_args()
+
+  jwt_secret = json.loads(os.environ['HASURA_GRAPHQL_JWT_SECRET'])
+  secret_key = jwt_secret['key']
+
+  user_id = args.user_id
+  role = args.role
+
+  token = create_token(secret_key=secret_key, user_id=user_id, role=role)
+  print(token)
